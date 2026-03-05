5d17f00cc6639b7ac1796c2a8b8379f0451c2999
Yaren <yarencnk03@gmail.com>
2026-03-05 15:27:17 +0300

    feat: setup Nginx reverse proxy with HTTPS and SQL Server config

--- Program.cs
+++ Program.cs
@@ -99,11 +99,8 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
